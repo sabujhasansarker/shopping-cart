6a1cc858bd3fff984b271249bedf01cca2a82019
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-08-29 01:27:40 +0600

    Updates

--- src/context/ShoppingContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-const ShoppingContext = createContext();
-export default ShoppingContext;
--- src/context/Type.js
+++
@@ -1,5 +0,0 @@
-export const CARD_ITEMS = "CARD_ITEMS";
-export const PRICE = "PRICE";
-export const SAVE = "SAVE";
-export const REMOVE = "REMOVE";
-export const CHECKOUT = "CHECKOUT";
