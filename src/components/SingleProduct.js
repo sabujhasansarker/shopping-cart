d23f88b57fe82f4c03508a41c0814c42cc0c3ae5
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-08-29 14:48:25 +0600

    single page

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -2,20 +2,63 @@ import React, { useContext, Fragment } from "react";
 import ShoppingContext from "../context/ShoppingContext";
 
 const SingleProduct = ({ match }) => {
-  const { items } = useContext(ShoppingContext);
+  const { items, cardItems, cards, setPrice, removeItem } = useContext(
+    ShoppingContext
+  );
+  // On click event
+  const onClick = (e) => {
+    cardItems(cards, e);
+    setPrice();
+  };
+  const cardTrue = cards.find((card) => card.id === Number(match.params.id));
+  const item =
+    cardTrue || items.find((card) => card.id === Number(match.params.id));
+
   return (
     <div className="single-product">
-      {items.map((item) => (
-        <Fragment key={item.id}>
-          {item.id === Number(match.params.id) && (
-            <div className="card">
-              <img src={item.image} alt="" />
-              <h1>{item.name}</h1>
-              <p>{item.dec}</p>
+      <div className="card" key={item.id}>
+        <img className="product-img" src={item.image} alt="" />
+        <div className="content">
+          <h1>{item.name}</h1>
+          <p className="dec">{item.dec}</p>
+          <p className="price">
+            <b>Price : </b>${item.price}
+          </p>
+          <p className="action-container">
+            <span
+              className="action"
+              onClick={() => cardItems(cards, item, "+")}
+            >
+              +
+            </span>
+            <span className="quantity">{item.qn}</span>
+            <span
+              className="action"
+              onClick={() => cardItems(cards, item, "-")}
+            >
+              -
+            </span>
+          </p>
+          <div className="flex">
+            <div className="button" onClick={() => onClick(item)}>
+              <img
+                src="https://img.icons8.com/plasticine/100/000000/add-shopping-cart.png"
+                alt=""
+              />
+              Add to cart
             </div>
-          )}
-        </Fragment>
-      ))}
+            {cardTrue && (
+              <div className="button remove" onClick={() => removeItem(item)}>
+                <img
+                  src="https://img.icons8.com/plasticine/100/000000/clear-shopping-cart.png"
+                  alt=""
+                />
+                Remove
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
