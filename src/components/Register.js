67ad669a8be3fb9d806b8dd14f81485d894cc242
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-08-30 19:59:16 +0600

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,26 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import { auth, database } from "../config/fire";
+
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password_1: "",
+    password_2: "",
+  });
+  const [error, setError] = useState(null);
+  const { name, email, password_1, password_2 } = data;
+  const onChange = (e) => setData({ ...data, [e.target.name]: e.target.value });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password_1 !== password_2) {
+      setError("Password dose not match");
+      return clearError();
+    }
+    try {
+      await auth.createUserWithEmailAndPassword(email, password_1);
+      await auth.currentUser.updateProfile({
+        displayName: name,
+      });
+    } catch (error) {
+      setError(error.message);
+      clearError();
+    }
+  };
+
+  const clearError = () => {
+    setTimeout(() => {
+      setError("");
+    }, 2000);
+  };
+
   return (
     <div className="register form-container">
       <h1>Register</h1>
-      <form action="" className="form">
+      <div className="alert">
+        <p>{error}</p>
+      </div>
+      <form action="" className="form" onSubmit={(e) => onSubmit(e)}>
         <div className="form-item">
           <label htmlFor="name">Name : </label>
-          <input type="text" name="name" placeholder="Name" />
+          <input
+            value={name}
+            onChange={(e) => onChange(e)}
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
         </div>
         <div className="form-item">
           <label htmlFor="email">Email : </label>
-          <input type="email" name="email" placeholder="E-mail" />
+          <input
+            value={email}
+            onChange={(e) => onChange(e)}
+            type="email"
+            name="email"
+            placeholder="E-mail"
+          />
         </div>
         <div className="form-item">
-          <label htmlFor="password_1">Password : </label>
-          <input type="password" name="password_1" placeholder="Password" />
+          <label htmlFor="password_1">Password :</label>
+          <input
+            value={password_1}
+            onChange={(e) => onChange(e)}
+            type="password"
+            name="password_1"
+            placeholder="Password"
+          />
         </div>
         <div className="form-item">
           <label htmlFor="password_2">Confirm Password : </label>
-          <input type="password" name="password_2" placeholder="Password" />
+          <input
+            value={password_2}
+            onChange={(e) => onChange(e)}
+            type="password"
+            name="password_2"
+            placeholder="Password"
+          />
         </div>
         <div className="form-item">
           <input className="btn" type="submit" name="submit" value="register" />
---
+++ src/config/fire.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD-ft5V9R9qLCoZpSCwFYgDhjYpa6HSZqs",
+  authDomain: "fir-319ab.firebaseapp.com",
+  databaseURL: "https://fir-319ab.firebaseio.com",
+  projectId: "fir-319ab",
+  storageBucket: "fir-319ab.appspot.com",
+  messagingSenderId: "728792064123",
+  appId: "1:728792064123:web:a9611443b0d159db5dc2de",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database().ref("user");
+export { auth, database };
